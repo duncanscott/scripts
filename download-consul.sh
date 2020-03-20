9d38c267481ee76452457f9c83e117a085170edd
Duncan Scott <dnscott@gmail.com>
2020-03-20 12:40:14 -0700

    added download-consul.sh

---
+++ download-consul.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+if [ -z "$CONSUL_VERSION" ]
+then
+  echo "Variable CONSUL_VERSION is not defined.  Exiting without installing Consul."
+  exit 1
+fi
+
+set -e
+
+echo "downloading Consul version $CONSUL_VERSION"
+
+dirname=consul_"$CONSUL_VERSION"
+dirpath="${HOME}"/downloads/consul/$dirname
+tmppath=/tmp/$dirname
+[ -d "$dirpath" ] && \rm -rf "$dirpath"
+[ -d "$tmppath" ] && \rm -rf "$tmppath"
+mkdir -p "${tmppath}"
+mkdir -p "${dirpath}"
+cd $tmppath
+
+curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA256SUMS
+curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA256SUMS.sig
+
+set +e
+shasum=`sha256sum consul_${CONSUL_VERSION}_linux_amd64.zip`
+echo "verifying sha256sum: ${shasum}"
+grep -q "${shasum}" consul_${CONSUL_VERSION}_SHA256SUMS
+if [ $? -eq 0 ]
+then
+  echo "sha256sum verified"
+else
+  echo "sha256sum verification FAILED"
+  exit 1
+fi
+
+set -e
+unzip consul_${CONSUL_VERSION}_linux_amd64.zip
+mv * "${dirpath}"
+echo "files downloaded to ${dirpath}"
+ls -l "${dirpath}"
+[ -d "$tmppath" ] && \rm -rf "$tmppath"
